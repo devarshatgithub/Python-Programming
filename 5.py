de1fa23128518ff74966cd63a48623cdc6ff354a
Devarsh <76740435+devarshatgithub@users.noreply.github.com>
2022-02-02 01:37:57 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,9 @@
+
+#   Id:20CE014_Contractor_Devarsh,
+#   Batch:CE-1, CSPIT
+
+# About: Program convert all lowercase letters to UPPERCASE letters and vice-versa.
+x = input("Enter String: ")
+if (x == ''):
+    x = 'HackerRank.com present \"Pythonnist 2\".'
+print(x.swapcase())     # swapcase() convert UPPERCASE characters to lowercase and vice-versa.
